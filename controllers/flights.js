0fcb7d8653cd9ae0288300407ecd9046bf68d079
ooh-lala <laurentozzi.code@gmail.com>
2023-01-11 22:24:49 -0500

    complete part 1

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,8 +1,6 @@
 import { Flight } from  "../models/flight.js"
 
 function index(req, res) {
-  console.log(req.date);
-  console.log(req.time)
   Flight.find({})
   .then(flights => {
     res.render('flights/index', {
@@ -21,27 +19,20 @@ function index(req, res) {
 
 function newFlight(req, res) {
   res.render("flights/new", {
-    airline: "Add Airline",
+    title: "Add Flight",
   })
  }
 
 
  function create(req, res) {
-  // convert checkbox of nothing or "on" to boolean
-  req.body.nowFlying = !!req.body.nowFlying
-  // replace and split if it's not an empty string
-  if (req.body.cast) {
-		// Key can be "title", "releaseYear", etc.
-    req.body.cast = req.body.cast.split(', ')
+  console.log("REQ.BODY", req.body)
+  for (let key in req.body) { 
+    if (req.body[key] === '') delete req.body[key]
   }
-  console.log(req.body);
-  for (const key in req.body) {
-	  if (req.body[key] === '') delete req.body[key]
-	}
-  console.log(req.body);
+  console.log("REQ.BODY", req.body)
   Flight.create(req.body)
   .then(flight => {
-    res.redirect(`/movies`)
+    res.redirect('/flights')
   })
   .catch(err => {
     res.redirect('/flights')
